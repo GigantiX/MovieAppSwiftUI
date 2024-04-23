c13dfdabc8963c615c15dc8126b2a3440de05e85
Axel Ganendra <intern.shavarel@gli.id>
2024-04-23 09:20:04 +0700

    Initial Commit

---
+++ FirstMonthSwiftUI/FirstMonthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstMonthSwiftUIApp.swift
+//  FirstMonthSwiftUI
+//
+//  Created by Axel Ganendra on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstMonthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
